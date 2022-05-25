6f2f9a10bb611209291f995e5b4a6c595c71418f
anaoliveira-studio <anajoliveira@gmail.com>
2022-05-25 10:46:42 +0200

    Création 1ère requête API + remise en place des commentaires HTML

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,22 @@
+const getProduct = async (url) => {
+    const response = await fetch(url)
+    if (response.status === 200) {
+        return await response.json();
+    } 
+    return 'error';
+}
 
+const showProduct = async () => {
+    const kanapList = await getProduct('http://localhost:3000/api/products');
+
+    if (kanapList === 'error') {
+        alert('Pas de produits disponibles');
+        return;
+    }
+
+    kanapList.forEach((kanap) => {
+        console.log(kanap);
+    }); 
+}
+
+showProduct();
